4e808ded3cec3cbc3253d9814dd77bf37e84d4f9
diogo <diogomendonca2015@outlook.com.br>
2023-08-06 12:13:31 -0300

    Criação primórdia as classes

---
+++ src/main/java/br/ufpb/dcx/comerciotech/EstoqueInterface.java
@@ -0,0 +1,9 @@
+package br.ufpb.dcx.comerciotech;
+
+public interface EstoqueInterface {
+
+    public void adicionarProduto();
+    public void removerProduto();
+    public boolean verificarDisponibilidade();
+
+}
---
+++ src/main/java/br/ufpb/dcx/comerciotech/Produto.java
@@ -0,0 +1,69 @@
+package br.ufpb.dcx.comerciotech;
+
+import java.util.Objects;
+
+public class Produto implements Comparable<Produto> {
+
+    private String nomeProduto;
+    private String idProduto;
+    private String fabricante;
+    private double preco;
+    private int qntNoEstoque;
+
+    public Produto(String nomeProduto, String idProduto, String fabricante, double preco, int qntNoEstoque) {
+        this.nomeProduto = nomeProduto;
+        this.idProduto = idProduto;
+        this.fabricante = fabricante;
+        this.preco = preco;
+        this.qntNoEstoque = qntNoEstoque;
+    }
+
+    public String getNomeProduto() {
+        return this.nomeProduto;
+    }
+
+    public String getIdProduto() {
+        return this.idProduto;
+    }
+
+    public String getFabricante() {
+        return this.fabricante;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQntNoEstoque() {
+        return this.qntNoEstoque;
+    }
+
+    public void setQntNoEstoque(int qntNoEstoque) {
+        this.qntNoEstoque = qntNoEstoque;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Double.compare(preco, produto.preco) == 0 && qntNoEstoque == produto.qntNoEstoque &&
+                Objects.equals(nomeProduto, produto.nomeProduto) && Objects.equals(idProduto, produto.idProduto) &&
+                Objects.equals(fabricante, produto.fabricante);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nomeProduto, idProduto, fabricante, preco, qntNoEstoque);
+    }
+
+    @Override
+    public int compareTo(Produto outroProduto) {
+        return Double.compare(this.preco, outroProduto.getPreco());
+    }
+
+}
