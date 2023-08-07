c8d15e1217213a7b9c6529d341ccb17d4b1e5121
diogo <diogomendonca2015@outlook.com.br>
2023-08-07 17:24:54 -0300

    Implementação de alguns métodos

--- src/main/java/br/ufpb/dcx/comerciotech/Carrinho.java
+++ src/main/java/br/ufpb/dcx/comerciotech/Carrinho.java
@@ -1,4 +1,33 @@
 package br.ufpb.dcx.comerciotech;
 
-public class Carrinho {
+import java.util.List;
+import java.util.LinkedList;
+
+public class Carrinho extends Estoque {
+
+    private List<Produto> produtosNoCarrinho;
+
+    public Carrinho() {
+        this.produtosNoCarrinho = new LinkedList<>();
+    }
+
+
+    public void adicionarProdutoNoCarrinho(Produto produto, int quantidadeASerAdicionada) throws ProdutoNaoEncontradoException {
+        if (!super.getProdutos().contains(produto)) {
+            throw new ProdutoNaoEncontradoException("Produto de ID " + produto.getIdProduto() + " não encontrado");
+        }
+        for (int i = 0; i < quantidadeASerAdicionada; i++) {
+            this.produtosNoCarrinho.add(produto);
+        }
+    }
+
+    public void removerProdutoNoCarrinho(Produto produto, int quantidadeASerRemovida) throws ProdutoNaoEncontradoException {
+        if (!super.getProdutos().contains(produto)) {
+            throw new ProdutoNaoEncontradoException("Produto de ID " + produto.getIdProduto() + " não encontrado");
+        }
+        for (int i = 0; i < quantidadeASerRemovida; i++) {
+            this.produtosNoCarrinho.remove(produto);
+        }
+    }
+
 }
