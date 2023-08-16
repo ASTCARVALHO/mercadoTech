aac294bcb82c41022d9d2905c9d5fa9e47889b0b
voidxzx <120760892+voidxzx@users.noreply.github.com>
2023-08-16 20:35:32 -0300

    Delete amigo secreto directory

--- amigo secreto/AmigoInexistenteException.java
+++
@@ -1,7 +0,0 @@
-package br.ufpb.dcx.diogo.amigosecreto;
-
-public class AmigoInexistenteException extends Exception {
-    public AmigoInexistenteException(String mensagem) {
-        super(mensagem);
-    }
-}
